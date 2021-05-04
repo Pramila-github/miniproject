267673cf986fc5d582f8d1d4ca275e1cf97df49e
Pramila-github <78983104+Pramila-github@users.noreply.github.com>
2021-05-05 01:08:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def set_png_as_page_bg(png_file):
 try:
     
     # set bg
-    set_png_as_page_bg('WindmillWallpaper_overall[1].jpg')
+    set_png_as_page_bg('WindmillWallpaper_tiff[1].jpg')
     
     # hide warning for st.pyplot() deprecation
     
