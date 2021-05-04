99031cb78e960f73cf8782af19187870d3194cc4
Pramila-github <78983104+Pramila-github@users.noreply.github.com>
2021-05-05 02:57:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def set_png_as_page_bg(png_file):
 try:
     
     # set bg
-    set_png_as_page_bg('gr.gif')
+    set_png_as_page_bg('01.gif')
     
     # hide warning for st.pyplot() deprecation
     
