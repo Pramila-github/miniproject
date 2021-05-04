e2a5d0ead2aceeaf7a76cecd3f5392cf09c88d59
Pramila-github <78983104+Pramila-github@users.noreply.github.com>
2021-05-04 22:53:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,49 +15,7 @@ page_icon="🚩",
 old_models = keras.models.load_model('model.h5')
 
 
-import base64
 
-# set background, use base64 to read local file
-def get_base64_of_bin_file(bin_file):
-    """
-    function to read png file 
-    ----------
-    bin_file: png -> the background image in local folder
-    """
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    """
-    function to display png as bg
-    ----------
-    png_file: png -> the background image in local folder
-    """
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-
-# app setup 
-try:    
-    # set bg
-    set_png_as_page_bg('000a.PNG')
-    
-    # hide warning for st.pyplot() deprecation
-    
-except TypeError:
-     st.error("Oops, something went wrong. Please check previous steps for inconsistent input.")
-        
 
 def home():
     return "welcome"
