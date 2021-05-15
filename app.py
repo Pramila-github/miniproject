8da86003d4c76940a4f97370ea5dbe831c2a25ee
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-05-16 03:05:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def main():
    if nav == "Home 🏡":
     st.markdown("<h1 style ='color:black; text_align:center;font-family:times new roman;font-size:20pt; font-weight: bold;'>DEEP WINDS ⚒️</h1>", unsafe_allow_html=True)
     st.markdown("<h1 style=' color:brown; text_align:center;font-weight: bold;font-size:19pt;'>Made by Quad Techies with ❤️</h1>", unsafe_allow_html=True)
-    st.markdown("<h1 style ='color:black; text_align:center;font-family:times new roman;font-weight: bold;font-size:16pt;'>🌐 WIND POWER PREDICTION DL WEB-APP 🌐</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style ='color:black; text_align:center;font-family:times new roman;font-weight: bold;font-size:16pt;'>🌎 WIND POWER PREDICTION DL WEB-APP 🌎</h1>", unsafe_allow_html=True)
     
    if nav == "User defined Prediction📟":
      set_png_as_page_bg('gra (1).jpg')
