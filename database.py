1bfb8e3302a786ef5648c966004a46cf7377bb68
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-05-28 03:48:54 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,32 @@
+import sqlite3 
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+def create_usertable():
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,comments TEXT)')
+
+def add_userdata(username,comments):
+	c.execute('INSERT INTO userstable(username,comments) VALUES (?,?)',(username,comments))
+	conn.commit()
+        
+def login_user(username,comments):
+ 	c.execute('SELECT * FROM userstable WHERE username =? AND comments = ?',(username,comments))
+ 	data = c.fetchall()
+ 	return data
+
+def select_all():
+    c.execute('SELECT * FROM userstable')
+    data1 = c.fetchall()
+    return data1
+
+def create_likestable():
+	c.execute('CREATE TABLE IF NOT EXISTS likestable(counts TEXT)')
+
+def add_likesdata(counts):
+	c.execute('INSERT INTO likestable(counts) VALUES (?)',(counts))
+	conn.commit()
+        
+
+def count_likes():
+    c.execute('SELECT count(*) FROM likestable')
+    data1 = c.fetchall()
+    return data1
