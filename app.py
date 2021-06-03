952d8f928e41199d2d507b0a677ed9821ae2ae95
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-06-04 01:24:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def main():
     with st.beta_expander("Like this page💰🏆!!"):
         if st.button("❤️"):
            #st.success("Thanks for your like 😀!")
-	   st.markdown("<h1 style='text-align: center; color:black ;background-color:powderblue;font-size:14pt'> Thanks for your like 😀! </h1>", unsafe_allow_html=True)
+	   st.markdown("<h1 style='text-align: center; color:black ;background-color:powderblue;font-size:14pt'>Thanks for your like 😀!</h1>", unsafe_allow_html=True)
            create_likestable()
            add_likesdata('1')
            like=count_likes()
