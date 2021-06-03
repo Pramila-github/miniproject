a09de03c133cc02ac3ed5508bd7a85be10c04192
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-06-04 01:42:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def main():
                 result = login_user(username,comments)
                 if result:
 		    #st.markdown("<h1 style='text-align: center; color:black ;background-color:powderblue;font-size:14pt'> {} </h1>".format(username), unsafe_allow_html=True)
-                    st.markdown("<h1 style='text-align: center; color:black;font-size:12pt'> Thankyou for your comment {} ❤️ - with regards Team DeepWind❤️ </h1>".format(username),unsafe_allow_html=True)
+                    st.markdown("<h1 style='text-align: center; color:white;font-size:12pt'> Thankyou for your comment {} ❤️ - with regards Team DeepWind❤️ </h1>".format(username),unsafe_allow_html=True)
     with st.beta_expander("View reviews 📝"):
           result=select_all()
           data=pd.DataFrame(result,columns=['UserName','Comments'])
