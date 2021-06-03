91ca5f1e91ce44b32b7b82b6957879f6ed6822c8
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-06-04 02:45:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def main():
             comments= st.text_input("Comments")
         if st.button("Post ✔️"):
             if((username is '')and(comments is '')):
-               st.markdown("<h1 style='text-align: left; font-weight: normal;color:red;background-color:white;font-size:12pt;border-style: solid;border-color:red;border-radius:4px'> Empty field </h1>".format(username),unsafe_allow_html=True) 
+               st.markdown("<h1 style='text-align: center; font-weight:bold;color:red;background-color:white;font-size:12pt;border-style: solid;border-color:red;border-radius:4px'> Empty field </h1>".format(username),unsafe_allow_html=True) 
             else:    
                 create_usertable()
                 add_userdata(username,comments)
