8cf458f219ec75b7167dfe7e1cd5a27327835d87
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-06-29 21:46:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def main():
      if st.button("Predict"):
          result = predict(temperature,pressure,wind_speed,wind_direction)
          st.balloons()  
-     st.success('Predicted Power is {} kW'.format("%.2f" % result)) 
+     st.success('Predicted Power is {} kW'.format(result)) 
      
         
    if nav == "Forecasting 📊":
